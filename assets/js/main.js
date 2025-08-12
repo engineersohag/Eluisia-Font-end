9d3331ae9735be138c0b76bd972b84f428a4582d
engineersohag <engineer.sohag.cse@gmail.com>
2025-08-12 16:26:14 +0600

    Home Page Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,34 @@
-$('.owl-carousel').owlCarousel({
+$(document).ready(function(){
+  // Home Page Carosel
+  $('.streaming-carousel').owlCarousel({
     loop:true,
     margin:15,
     autoplay:true,
     autoplayTimeout:2500,
     autoplayHoverPause:true,
     responsive:{
-      0:{ items:1 },
+      0:{ items:2 },
       576:{ items:2 },
       768:{ items:3 },
       992:{ items:4 },
       1200:{ items:5 }
     }
   });
+
+  $('.nostalgia-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    responsive:{
+      0:{ items:2 },
+      576:{ items:2 },
+      768:{ items:4 },
+      992:{ items:5 },
+      1200:{ items:6 }
+    }
+  });
+});
+
+
